57a188b5f8ae00bc9677b975973e4eea8d5326ff
Yuri Schimke <yuri@schimke.ee>
2026-09-19 22:01:14 +0100

    feat(ui-builder): render desktop previews remotely (#56)
    * feat(ui-builder): render desktop previews remotely
    
    * fix(ui-builder): allow loopback preview servers
    
    * fix(ui-builder): keep remote previews current

--- ui-builder-desktop/build.gradle.kts
+++ ui-builder-desktop/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 plugins {
   alias(libs.plugins.ktfmt)
   alias(libs.plugins.kotlin.multiplatform)
+  alias(libs.plugins.kotlin.serialization)
   alias(libs.plugins.compose.multiplatform)
   alias(libs.plugins.compose.compiler)
 }
